a8615fc07b671ad465947f2dc91a98e9813698e3
MagickOrange <452959354@qq.com>
2024-05-02 01:49:35 +0800

    v0.2.37 新增成长系统UI表现。

---
+++ Assets/Scripts/Game/UIPowerUp.Designer.cs
@@ -0,0 +1,20 @@
+// Generate Id:1209777b-188e-4bcc-ae89-10467fca2dbf
+using UnityEngine;
+
+namespace ProjectindieFarm
+{
+	public partial class UIPowerUp
+	{
+
+		public UnityEngine.UI.Button BtnHandRange1;
+
+		public UnityEngine.UI.Button BtnShovelRange1;
+
+		public UnityEngine.UI.Button BtnWateringCan1;
+
+		public UnityEngine.UI.Button BtnSellFruit;
+
+		public UnityEngine.UI.Button BtnSellRadish;
+
+	}
+}
